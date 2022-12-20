c2c98abb312785e6f06e7df8b2d11cff36b982e5
zeeshaninovio <112846471+maviaakhan@users.noreply.github.com>
2022-12-20 22:54:20 +0500

    create about page

---
+++ about.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" href="css/style.css">
+<!-- App Include Here -->
+<?php include 'inc/app.php';?>
+<!-- App Include Here -->
+
+<!-- Body Content Start Here -->
+<section>
+    <div class="container">
+        <h1>About Page</h1>
+    </div>
+</section>
+<!-- Body Content End Here -->
+
+<!-- Footer Include Here -->
+<?php include 'inc/footer.php';?>
+<!-- Footer Include Here -->
